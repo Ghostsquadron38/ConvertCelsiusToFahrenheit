9c052b8a872d48ba05bfbc2af26e1e89f249ae88
Ghostsquadron38 <dokhanhsang159753@gmail.com>
2020-12-20 20:49:21 +0700

    first commit

---
+++ ConvertCtoF/ConvertCtoF.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     double f,c;
+     cout<<"Input the temperature in Celsius scale: "; cin>>c;
+     f = (c*(9/5.0))+32;
+     cout<<"The temperature in Fahrenheit scale: "<<f;
+     return 0;
+}
